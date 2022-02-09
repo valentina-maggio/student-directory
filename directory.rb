601f49d1693daef00f4a4642acc5bd2c952ed1fd
Valentina <maggio.vale@gmail.com>
2022-02-09 18:22:32 +0000

    Updated directory file with comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#First we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Normal Bates"
+#Finally, we print the total number of students
 print "Overall, we have "
+#It's important that print doesn't add new lina characters
 print 11
 puts " great students"
