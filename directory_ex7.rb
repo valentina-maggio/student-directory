518919d989c7b7bb59c2f8d07734bf861a9a4f94
Valentina <maggio.vale@gmail.com>
2022-02-10 18:40:13 +0000

    Updated exercise #7 with typo scenario

--- directory_ex7.rb
+++ directory_ex7.rb
@@ -2,22 +2,29 @@ def input_students
   puts "Please for each student enter the name"
   students = []
   student = gets.chomp
-
   puts "And now enter the cohort"
   cohort = gets.chomp
   # Populate the student array with student name and cohort (if cohort not specified, will add 'NotSpecified')
-  puts "To finish, juts hit return twice."
   while !student.empty? do
+  # When there is a typo the user can re-enter the inputs  
+    puts "If you made a typo, please enter 'typo', else keep entering the student name and cohort"
+    typo = gets.chomp
+    if typo == 'typo'
+      puts "Please re-enter the student name"
+      student = gets.chomp
+      puts "Please re-enter the cohort"
+      cohort = gets.chomp
+    end
     if cohort.empty?
       students << {name: student, cohort: :NotSpecified}
     else
       students << {name: student, cohort: cohort.to_sym}
     end
     puts "Now we have #{students.count} students"
+    puts "To finish, juts hit return twice, else keep entering the students name and cohort."
     student = gets.chomp
     cohort = gets.chomp
   end
-
   students
 end
 
