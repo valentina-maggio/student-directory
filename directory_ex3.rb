2ab56d5b40adeebb60b70f38ec306589a878e5fd
Valentina <maggio.vale@gmail.com>
2022-02-10 10:44:31 +0000

    Copy of directory file with exercise #3

---
+++ directory_ex3.rb
@@ -0,0 +1,40 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, juts hit return twice"
+  # Create an empty array
+  students = []
+  # Get the first name
+  name = gets.chomp
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    if student[:name].size < 12
+      puts "#{student[:name]}"
+    end
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+# Nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
