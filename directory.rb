0a7d188ff9edd2fb2bcde2cc9753dfa21287bc96
Valentina <maggio.vale@gmail.com>
2022-02-11 17:26:58 +0000

    Added method to load students data from csv file

--- directory.rb
+++ directory.rb
@@ -22,6 +22,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # we'll be adding more items
 end
 
@@ -33,6 +34,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -71,6 +74,15 @@ def save_students
     file.puts csv_line
   end
   # Close the file
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
   file.close
 end
 
